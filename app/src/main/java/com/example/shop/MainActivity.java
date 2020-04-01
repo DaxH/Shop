0202d503083bfac1527f4aa97120695c2f80721e
DaxH <52769764+DaxH@users.noreply.github.com>
2020-04-01 08:49:27 -0500

    Menu terminado

--- app/src/main/java/com/example/shop/MainActivity.java
+++ app/src/main/java/com/example/shop/MainActivity.java
@@ -1,14 +1,34 @@
 package com.example.shop;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    Toolbar toolbar;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+        drawerLayout = findViewById(R.id.drawer);
+        navigationView = findViewById(R.id.navigation_view);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
+        actionBarDrawerToggle.syncState();
+
+
     }
 }
---
+++ app/src/main/java/com/example/shop/Vista/Fragments/MainFragmen.java
@@ -0,0 +1,6 @@
+package com.example.shop.Vista.Fragments;
+
+import androidx.fragment.app.Fragment;
+
+public class MainFragmen extends Fragment {
+}

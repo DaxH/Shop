219b5a15e398de8a045f88ab218c057eb35fffd4
SteveGuillermoAC <falloutk786@gmail.com>
2020-04-03 12:49:50 -0500

    newBranch

--- app/src/main/java/com/example/shop/Vista/Fragments/MainFragmen.java
+++
@@ -1,6 +0,0 @@
-package com.example.shop.Vista.Fragments;
-
-import androidx.fragment.app.Fragment;
-
-public class MainFragmen extends Fragment {
-}
---
+++ app/src/main/java/com/example/shop/Vista/Login.java
@@ -0,0 +1,5 @@
+package com.example.shop.Vista;
+
+public class Login {
+
+}

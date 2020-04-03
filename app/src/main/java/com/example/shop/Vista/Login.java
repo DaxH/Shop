609d0c58e665da737c2003b9ca50b9de2e165015
SteveGuillermoAC <falloutk786@gmail.com>
2020-04-03 18:45:23 -0500

    login xml

--- app/src/main/java/com/example/shop/Vista/Login.java
+++ app/src/main/java/com/example/shop/Vista/Login.java
@@ -1,5 +1,17 @@
 package com.example.shop.Vista;
 
-public class Login {
+import android.os.Bundle;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.shop.R;
+
+public class Login extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+
+    }
 }

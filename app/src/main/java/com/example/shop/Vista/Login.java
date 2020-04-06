5df18a1a5a8fa3ee8434e11463ca12d091f4f5ff
DaxH <52769764+DaxH@users.noreply.github.com>
2020-04-05 20:25:12 -0500

    Fix vista login terminado

--- app/src/main/java/com/example/shop/Vista/Login.java
+++ app/src/main/java/com/example/shop/Vista/Login.java
@@ -1,6 +1,8 @@
 package com.example.shop.Vista;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,5 +15,12 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
 
+    }
+
+    public void VistaPrincipal(View vista){
+//        Al iniciar secion cambia a la venta principal de la apliacion
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
